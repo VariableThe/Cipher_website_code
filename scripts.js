39a0fbfa6e9560be2ef0260bb0a80c74d709e533
Aditya Sharma <slayermk57@gmail.com>
2024-08-20 18:37:23 +0530

    addition of better cipher

--- scripts.js
+++ scripts.js
@@ -1,43 +1,24 @@
-function processROT3(action) {
-    const inputText = document.getElementById('rot3InputText').value.toUpperCase();
-    let result = '';
-    if (action === 'encode') {
-        result = encodeROT3(inputText);
-    } else if (action === 'decode') {
-        result = decodeROT3(inputText);
-    }
-    document.getElementById('rot3Result').textContent = 'Result: ' + result;
-}
-
-function encodeROT3(text) {
-    return text.replace(/[A-Z]/g, char =>
-        String.fromCharCode((char.charCodeAt(0) - 65 + 3) % 26 + 65)
-    );
-}
-
-function decodeROT3(text) {
-    return text.replace(/[A-Z]/g, char =>
-        String.fromCharCode((char.charCodeAt(0) - 65 - 3 + 26) % 26 + 65)
-    );
-}
-
 function processVigenere(action) {
     const message = document.getElementById('vigenereInputText').value.toUpperCase();
     const key = document.getElementById('vigenereKeyText').value.toUpperCase();
     let result = '';
+
     if (action === 'encode') {
         result = encodeVigenere(message, key);
     } else if (action === 'decode') {
         result = decodeVigenere(message, key);
     }
+
     document.getElementById('vigenereResult').textContent = 'Result: ' + result;
 }
 
 function encodeVigenere(message, key) {
     let encodedMessage = '';
+
     for (let i = 0; i < message.length; i++) {
         let messageChar = message.charCodeAt(i);
         let keyChar = key.charCodeAt(i % key.length);
+
         if (messageChar >= 65 && messageChar <= 90) {
             let shift = (messageChar - 65 + keyChar - 65) % 26;
             let encodedChar = String.fromCharCode(shift + 65);
@@ -46,14 +27,17 @@ function encodeVigenere(message, key) {
             encodedMessage += message[i];
         }
     }
+
     return encodedMessage;
 }
 
 function decodeVigenere(message, key) {
     let decodedMessage = '';
+
     for (let i = 0; i < message.length; i++) {
         let messageChar = message.charCodeAt(i);
         let keyChar = key.charCodeAt(i % key.length);
+
         if (messageChar >= 65 && messageChar <= 90) {
             let shift = (messageChar - 65 - (keyChar - 65) + 26) % 26;
             let decodedChar = String.fromCharCode(shift + 65);
@@ -62,5 +46,57 @@ function decodeVigenere(message, key) {
             decodedMessage += message[i];
         }
     }
+
+    return decodedMessage;
+}
+
+function processAutokey(action) {
+    const message = document.getElementById('autokeyInputText').value.toUpperCase();
+    const key = document.getElementById('autokeyKeyText').value.toUpperCase();
+    let result = '';
+
+    if (action === 'encode') {
+        result = encodeAutokey(message, key);
+    } else if (action === 'decode') {
+        result = decodeAutokey(message, key);
+    }
+
+    document.getElementById('autokeyResult').textContent = 'Result: ' + result;
+}
+
+function encodeAutokey(message, key) {
+    let encodedMessage = key;
+    for (let i = 0; i < message.length; i++) {
+        let messageChar = message.charCodeAt(i);
+        let keyChar = encodedMessage.charCodeAt(i);
+
+        if (messageChar >= 65 && messageChar <= 90) {
+            let shift = (messageChar - 65 + keyChar - 65) % 26;
+            let encodedChar = String.fromCharCode(shift + 65);
+            encodedMessage += encodedChar;
+        } else {
+            encodedMessage += message[i];
+        }
+    }
+    return encodedMessage.substring(key.length);
+}
+
+function decodeAutokey(message, key) {
+    let decodedMessage = '';
+    let combinedText = key + message;
+
+    for (let i = 0; i < message.length; i++) {
+        let messageChar = message.charCodeAt(i);
+        let keyChar = combinedText.charCodeAt(i);
+
+        if (messageChar >= 65 && messageChar <= 90) {
+            let shift = (messageChar - 65 - (keyChar - 65) + 26) % 26;
+            let decodedChar = String.fromCharCode(shift + 65);
+            decodedMessage += decodedChar;
+        } else {
+            decodedMessage += message[i];
+        }
+    }
+
     return decodedMessage;
 }
